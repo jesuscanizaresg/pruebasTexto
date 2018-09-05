6e3d7024335b030ac0558420dc802cb4791c3511
DESKTOP-NF3UADG\Jesus-Pc <jesus.canizares@qualityprovider.net>
2018-09-05 12:56:03 +0200

    Antes de prueba GOOGLE

--- src/main.java
+++ src/main.java
@@ -1,12 +1,38 @@
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.JOptionPane;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.tika.Tika;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.ocr.TesseractOCRConfig;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.parser.pdf.PDFParserConfig;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
+import com.google.common.io.Files;
+import com.healthmarketscience.jackcess.util.ImportUtil;
 
 public class main {
 
@@ -277,6 +303,201 @@ public class main {
 		
 		System.out.println(pagina);
 		
+		
+		texto = "https://sede.seg-social.gob.es/wps/portal/sede/sede/TablonAnuncios/!ut/p/z1/jY9NT8MwDIZ_yw49ErvL6Bpu0YrQtqKqGoEulymVsmyoNFUa1sGvJ8CJ8TXfbD2P7RckVCBbddgb5fe2VU3o1zLZ0DiZxAzj_CbLZ8jF_ZKypBxjivBwAuD8OgC3S8FoSZEhyHN8_KX4v_5K92GHPMG-f_EB_HFmAdI0tv6MzNuapgak01vttCPPLox33nf9VYQRDsNAjLWm0UT3Ef5k7GzvofoCwjpEnW4uixTjBY7zQmQT5GzGSpEViMUUVu9vPqqDOpLOOt9oT1TQELonUR3nF7J-oa93fDR6AybpG3A!/dz/d5/L2dBISEvZ0FBIS9nQSEh/p0/IZ7_5O801J02LOUD40A9C9QUDO00O7=CZ6_31641901LGDLC0AUVK396Q2080=LA0=Epaginar!2==/#Z7_5O801J02LOUD40A9C9QUDO00O7";
+		p = Pattern.compile("==/");
+		m = p.matcher(texto);
+		if(m.find()) {
+			System.out.println(texto);
+			String identificador = StringUtils.substringAfter(texto,m.group(0));
+			System.out.println(identificador);
+		}
+		
+		System.out.println("Salida con librerias BodycontenHandler: ************************************************************");
+		//TEST tika
+		/*** Tika ***/
+		BodyContentHandler handler = new BodyContentHandler();
+		Metadata metadata = new Metadata();
+		FileInputStream inputstream = null;
+		try {
+			inputstream = new FileInputStream("C:\\Temp\\DescargaBOP\\Albacete\\2018\\79-6_(06_07_2018).pdf");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ParseContext pcontext = new ParseContext();
+		
+		//org.apache.tika.parser.pdf.PDFParser parser1 = new org.apache.tika.parser.pdf.PDFParser();
+		Parser parser1 = new AutoDetectParser();
+		try {
+			parser1.parse(inputstream, handler, metadata,pcontext);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TikaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(new File("C:\\Temp\\Parser_tika_org.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		out.println("Contenido del PDF: \r\n"
+				+ "BodyContentHandler handler = new BodyContentHandler();"+"\r\n"
+				+ "FileInputStream inputstream = null;"+"\r\n"
+				+ "Metadata metadata = new Metadata();"+"\r\n"
+				+ "Parser parser1 = new AutoDetectParser();"+"\r\n"
+				+ "parser1.parse(inputstream, handler, metadata,pcontext);"+"\r\n"
+				+ "TEXTO EXTRAIDO Con este procedimiento: -------------------------------------->"+"\r\n"
+				+handler.toString());
+		out.close();
+		
+		System.out.println("Salida con Clase TIKA: *************************************************");
+		Tika tika = new Tika();
+		tika.setMaxStringLength(-1);
+		PrintWriter salida = null;
+		try {
+			salida = new PrintWriter(new File("C:\\Temp\\Tika_class.txt"));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			texto = tika.parseToString(new File("C:\\Temp\\DescargaBOP\\Albacete\\2018\\82-9_(13_07_2018).pdf"));
+			//System.out.println(texto);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TikaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		salida.print("Contenido del PDF: \r\n"
+				+ "BodyContentHandler handler = new BodyContentHandler();"+"\r\n"
+				+ "FileInputStream inputstream = null;"+"\r\n"
+				+ "Metadata metadata = new Metadata();"+"\r\n"
+				+ "Parser parser1 = new AutoDetectParser();"+"\r\n"
+				+ "parser1.parse(inputstream, handler, metadata,pcontext);"+"\r\n"
+				+ "TEXTO EXTRAIDO Con este procedimiento: -------------------------------------->"+"\r\n"
+				+ texto);
+		salida.close();
+		
+		
+		System.out.println("Salida con PDFParser forzado**************************************");
+		
+		//TEST tika forzando PDFparser
+				/*** Tika ***/
+				handler = new BodyContentHandler();
+				metadata = new Metadata();
+				inputstream = null;
+				try {
+					inputstream = new FileInputStream("C:\\Temp\\DescargaBOP\\Albacete\\2018\\79-6_(06_07_2018).pdf");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				pcontext = new ParseContext();
+				
+				//org.apache.tika.parser.pdf.PDFParser parser1 = new org.apache.tika.parser.pdf.PDFParser();
+				PDFParser parser2 = new PDFParser();
+				try {
+					parser2.parse(inputstream, handler, metadata,pcontext);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SAXException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (TikaException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				PrintWriter out_pdfparser = null;
+				try {
+					out_pdfparser = new PrintWriter(new File("C:\\Temp\\PdfParser_tika_org.txt"));
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				out_pdfparser.println("Contenido del PDF: \r\n"
+						+ "BodyContentHandler handler = new BodyContentHandler();"+"\r\n"
+						+ "FileInputStream inputstream = null;"+"\r\n"
+						+ "Metadata metadata = new Metadata();"+"\r\n"
+						+ "PDFParser parser2 = new PDFParser();"+"\r\n"
+						+ "parser2.parse(inputstream, handler, metadata,pcontext);"+"\r\n"
+						+ "TEXTO EXTRAIDO Con este procedimiento: -------------------------------------->"+"\r\n"
+						+handler.toString());
+				out_pdfparser.close();
+				
+				System.out.println("Salida con Librerias OCR *****************************************************");
+				
+				InputStream pdfOcr = null;
+				try {
+					pdfOcr = new FileInputStream("C:\\Users\\Jesus-Pc\\Desktop\\Madrid_ERROR\\144-140_(18_06_2018).pdf");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				ByteArrayOutputStream outByORC = new ByteArrayOutputStream();
+				TikaConfig tikaConfig = TikaConfig.getDefaultConfig();
+				BodyContentHandler handlerOcr = new BodyContentHandler(outByORC);
+				Parser parserOCR = new AutoDetectParser(tikaConfig);
+				Metadata metaOCR = new Metadata();
+				ParseContext parseContext = new ParseContext();
+				
+				PDFParserConfig pdfConfigOCR = new PDFParserConfig();
+				pdfConfigOCR.setExtractInlineImages(true);
+				
+				TesseractOCRConfig tesserConfig = new TesseractOCRConfig();
+				tesserConfig.setLanguage("eng");
+				tesserConfig.setTessdataPath("C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe");
+				
+				parseContext.set(Parser.class, parserOCR);
+				parseContext.set(PDFParserConfig.class, pdfConfigOCR);
+				parseContext.set(TesseractOCRConfig.class, tesserConfig);
+				try {
+					parserOCR.parse(pdfOcr, handlerOcr, metaOCR, parseContext);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SAXException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (TikaException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				//System.out.println(new String(outByORC.toByteArray(), Charset.defaultCharset()));
+				
+				OutputStream ops = null;
+				try {
+					ops = new FileOutputStream("C:\\Temp\\TesseractOCR_tika_org.txt");
+					outByORC.writeTo(ops);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}finally {
+					try {
+						outByORC.close();
+						ops.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			
+
+			System.out.println("PROGRAMA FINALIZADO.......");	
 	}
 	
 	
